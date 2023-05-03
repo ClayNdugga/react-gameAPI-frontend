4e43812fa53c67b1cabbc677d58ce0cb0f51d75a
clayndugga <clayndugga@gmail.com>
2023-05-02 18:43:18 -0600

    fixed infinte query

--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,35 +1,35 @@
-import { useEffect, useState } from "react";
-import apiClient, { FetchResponse } from "../services/api-client";
-import { AxiosRequestConfig, CanceledError } from "axios";
+// import { useEffect, useState } from "react";
+// import apiClient, { FetchResponse } from "../services/api-client";
+// import { AxiosRequestConfig, CanceledError } from "axios";
 
 
 
 
-const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {  //? makes request optional
-    const [data, setData] = useState<T[]>([])
-    const [error, setError] = useState("")
-    const [isLoading, setLoading] = useState(false)
+// const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {  //? makes request optional
+//     const [data, setData] = useState<T[]>([])
+//     const [error, setError] = useState("")
+//     const [isLoading, setLoading] = useState(false)
 
-    useEffect(() => {
-        const controller = new AbortController()
+//     useEffect(() => {
+//         const controller = new AbortController()
 
-        setLoading(true)
-        apiClient
-            .get<FetchResponse<T>>(endpoint, {...requestConfig, signal: controller.signal})    //signal contains axios config config object
-            .then((res) => {
-                setData(res.data.results)
-                setLoading(false)
-            })
-            .catch((err) => {
-                if (err instanceof CanceledError) return
-                setError(err.message)
-                setLoading(false)
-            })
+//         setLoading(true)
+//         apiClient
+//             .get<FetchResponse<T>>(endpoint, {...requestConfig, signal: controller.signal})    //signal contains axios config config object
+//             .then((res) => {
+//                 setData(res.data.results)
+//                 setLoading(false)
+//             })
+//             .catch((err) => {
+//                 if (err instanceof CanceledError) return
+//                 setError(err.message)
+//                 setLoading(false)
+//             })
 
-        return () => controller.abort()
-    }, deps ? [...deps] : [])
+//         return () => controller.abort()
+//     }, deps ? [...deps] : [])
 
-    return {data, isLoading, error}
-}
+//     return {data, isLoading, error}
+// }
 
-export default useData;
+// export default useData;
--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,25 +1,27 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient, { FetchResponse } from "../services/api-client";
 
-import genres from "../data/genres";
-
-const apiClient = new APIClient<Genre>("/genres");
+// import genres from "../data/genres";
 
 export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
+    id: number;
+    name: string;
+    image_background: string;
+  }
+
+  const apiClient = new APIClient<Genre>("/genres");
+
+
 
 // const useGenres = () => ({data:genres, isLoading: false, error:null})
 // const useGenres = () => useData<Genre>('/genres')
 
 const useGenres = () =>
-  useQuery({
-    queryKey: ["genres"],
+  useQuery<FetchResponse<Genre>, Error>({
+    queryKey: ['genres'],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000, //24 hours
-    initialData: { count: genres.length, results: genres },
+    // initialData: { count: genres.length, results: genres },
   });
 
 export default useGenres;
